b6ba7fa30fcbdc0431637c304cfdba76c503529e
Steve <steven.conger@seattlecolleges.edu>
2018-06-11 11:57:40 -0700

    Added New Product

---
+++ BakeryProject/Controllers/NewProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryProject.Models;
+
+namespace BakeryProject.Controllers
+{
+    public class NewProductController : Controller
+    {
+        // GET: NewProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include ="ProductName, ProductPrice")]Product p)
+        {
+            BakeryEntities db = new BakeryEntities();
+            db.Products.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+
+
+
+    }
+}
--- BakeryProject/Controllers/SaleController.cs
+++ BakeryProject/Controllers/SaleController.cs
@@ -22,61 +22,62 @@ namespace BakeryProject.Controllers
                 return RedirectToAction("Result", m);
             }
             //Create a Sales Order Object
-            {
-                Sale s = new Sale();
-                Session["Sale"] = s;
+            //    {
+            //        Sale s = new Sale();
+            //        Session["Sale"] = s;
 
-                ViewBag.products = new SelectList(db.Products, "ProductKey", "ProductName");
-                return View();
-                }
-            }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Index([Bind(Include = "ProductKey,ProductName,Price,Quantity, Discount")]Item i)
-        { 
-       var prod = from p in db.Products
-                  where p.ProductKey == i.ProductKey
-                  select new { p.ProductName, p.ProductPrice };
+            //        ViewBag.products = new SelectList(db.Products, "ProductKey", "ProductName");
+                 return View();
+               }
+            //}
+            //[HttpPost]
+            //[ValidateAntiForgeryToken]
+            //        public ActionResult Index([Bind(Include = "ProductKey,ProductName,Price,Quantity, Discount")]Item i)
+            //        { 
+            //       var prod = from p in db.Products
+            //                  where p.ProductKey == i.ProductKey
+            //                  select new { p.ProductName, p.ProductPrice };
 
-            foreach(var pr in prod)
-            {
-                i.ProductName = pr.ProductName.ToString();
-                i.Price = (decimal) pr.ProductPrice;
-    }
-            Sale s = (Sale)Session["Sale"];
+            //            foreach(var pr in prod)
+            //            {
+            //                i.ProductName = pr.ProductName.ToString();
+            //                i.Price = (decimal) pr.ProductPrice;
+            //    }
+            //            Sale s = (Sale)Session["Sale"];
 
-            s.AddItem(i);
-            Session["Sale"] = s;
-            ViewBag.prduct = new SelectList(db.Products, "ProductKey", "ProductName");
-            return View();
-}
-        public ActionResult FinishSale()
+            //            s.AddItem(i);
+            //            Session["Sale"] = s;
+            //            ViewBag.prduct = new SelectList(db.Products, "ProductKey", "ProductName");
+            //            return View();
+            //}
+            //        public ActionResult FinishSale()
 
-           {
-            Sale sale = new Sale();
-            sale.EmployeeKey = 1;
-            sale.SaleDate = DateTime.Now;
-            sale.CustomerKey = (int)Session["PersonKey"];
-            db.Sales.Add(sale);
+            //           {
+            //            Sale sale = new Sale();
+            //            sale.EmployeeKey = 1;
+            //            sale.SaleDate = DateTime.Now;
+            //            sale.CustomerKey = (int)Session["PersonKey"];
+            //            db.Sales.Add(sale);
 
 
-            Sale s = (Sale)Session["sale"];
-            List<Item> saleItem = s.GetItem();
+            //            Sale s = (Sale)Session["sale"];
+            //            List<Item> saleItem = s.GetItem();
 
-            foreach (Item in saleItem)
-            {
-                SaleDetail sd = new SaleDetail();
-                sd.Sale = sale;
-                sd.ProductKey = i.ProductKey;
-                sd.SaleDetailPriceCharged = i.Price;
-                sd.SaleDetailQuantity = i.Quantity;
-                sd.SaleDetailDiscount = (decimal)i.Discount;
-                sd.SaleDetailSaleTaxPercent = .09m;
-                sd.SaleDetailEatInTax = .01m;
+            //            foreach (Item in saleItem)
+            //            {
+            //                SaleDetail sd = new SaleDetail();
+            //                sd.Sale = sale;
+            //                sd.ProductKey = i.ProductKey;
+            //                sd.SaleDetailPriceCharged = i.Price;
+            //                sd.SaleDetailQuantity = i.Quantity;
+            //                sd.SaleDetailDiscount = (decimal)i.Discount;
+            //                sd.SaleDetailSaleTaxPercent = .09m;
+            //                sd.SaleDetailEatInTax = .01m;
 
-                db.SaleDetails.Add(sd);
-            }
+            //                db.SaleDetails.Add(sd);
+            //            }
 
 
 
         }
+    }
